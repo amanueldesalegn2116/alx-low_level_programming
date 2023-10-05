ab2bae5d418f8270c523a7a7665bc6ef4ba42a5d
amanueldesalegn2116 <amanueldesalegn2116@gmail.com>
2023-10-05 13:36:23 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - a program will print the line as puts function
+ *
+ *void - the default value for main
+ *
+ *Return:  return value must be  0
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
