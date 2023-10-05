9af220198c296042696e9c98207fe2cabf17eccd
amanueldesalegn2116 <amanueldesalegn2116@gmail.com>
2023-10-05 13:42:17 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - the main function
+ * Return: must be "0" always
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
