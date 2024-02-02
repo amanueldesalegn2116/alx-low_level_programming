06c636ef988ba1164f2649024b6c45fcebc88170
amanueldesalegn2116 <amanueldesalegn2116@gmail.com>
2024-02-02 02:56:25 -0800

    Hash table

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+/**
+ * hash_djb2 - implementation djb2 algorithm
+ * @str: string
+ *
+ * Return: value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
