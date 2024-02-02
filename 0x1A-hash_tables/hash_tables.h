ba562f30a21492572e2aeb743fe7ff5d5883bd88
amanueldesalegn2116 <amanueldesalegn2116@gmail.com>
2024-02-02 03:10:46 -0800

    hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,11 +35,11 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-hash_node_t* create_new_node(const char* key, const char* value);
-int hash_table_set(hash_table_t* ht, const char* key, const char* value);
-char* hash_table_get(const hash_table_t* ht, const char* key);
-void hash_table_print(const hash_table_t* ht);
-void hash_table_delete(hash_table_t* ht);
+hash_node_t *create_new_node(const char *key, const char *value);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -58,11 +58,11 @@ void free_hash_list(hash_node_t *head);
  */
 typedef struct shash_node_s
 {
-     char *key;
-     char *value;
-     struct shash_node_s *next;
-     struct shash_node_s *sprev;
-     struct shash_node_s *snext;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -77,10 +77,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-     unsigned long int size;
-     shash_node_t **array;
-     shash_node_t *shead;
-     shash_node_t *stail;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
